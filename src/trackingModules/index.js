8054e6f9231399ba3bde593df988b83a215b6809
Beatriz Mendes <bea.smendes98@gmail.com>
2023-04-17 17:41:54 +0200

    feat: add palette tracking
    Closes #7

--- src/trackingModules/index.js
+++ src/trackingModules/index.js
@@ -1,8 +1,10 @@
+import palette from './palette';
 import popupMenuTracking from './popupMenu';
 import selectionTracking from './selection';
 
 export default {
   __depends__: [
+    palette,
     popupMenuTracking,
     selectionTracking
   ]
---
+++ src/trackingModules/palette/PaletteTracking.js
@@ -0,0 +1,37 @@
+import { getEventType } from '../Util';
+
+export default class Palette {
+  constructor(eventBus, bpmnJSTracking, selection) {
+    this._eventBus = eventBus;
+    this._bpmnJSTracking = bpmnJSTracking;
+    this._selection = selection;
+
+    this._eventBus.on('palette.trigger', this.trackTrigger.bind(this));
+  }
+
+  trackTrigger(e) {
+    const { entry, event } = e;
+    const { target } = event;
+
+    const entryAction = target.getAttribute('data-action');
+
+    const type = getEventType(event);
+
+    this._bpmnJSTracking.track({
+      name: 'palette.trigger',
+      data: {
+        entryId: entryAction,
+        entryGroup: entry.group || 'default',
+        entryTitle: entry.title,
+        selection: this._selection.get(),
+        triggerType: type
+      }
+    });
+  }
+}
+
+Palette.$inject = [
+  'eventBus',
+  'bpmnJSTracking',
+  'selection'
+];
