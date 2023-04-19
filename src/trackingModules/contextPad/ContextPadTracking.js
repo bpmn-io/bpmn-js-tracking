fe054a8ef75187713de9a744c86749b0440b4ece
Beatriz Mendes <bea.smendes98@gmail.com>
2023-04-19 10:51:11 +0200

    feat: add context pad tracking
    Closes #14

---
+++ src/trackingModules/contextPad/ContextPadTracking.js
@@ -0,0 +1,37 @@
+import { getEventType } from '../Util';
+
+export default class PopupMenuTracking {
+  constructor(eventBus, bpmnJSTracking, selection) {
+    this._eventBus = eventBus;
+    this._bpmnJSTracking = bpmnJSTracking;
+    this._selection = selection;
+
+    this._eventBus.on('contextPad.trigger', this.trackTrigger.bind(this));
+  }
+
+  trackTrigger(e) {
+    const { entry, event } = e;
+    const { target } = event;
+
+    const entryAction = target.getAttribute('data-action');
+
+    const type = getEventType(event);
+
+    this._bpmnJSTracking.track({
+      name: 'contextPad.trigger',
+      data: {
+        entryId: entryAction,
+        entryGroup: entry.group || 'default',
+        entryTitle: entry.title,
+        selection: this._selection.get(),
+        triggerType: type
+      }
+    });
+  }
+}
+
+PopupMenuTracking.$inject = [
+  'eventBus',
+  'bpmnJSTracking',
+  'selection'
+];
